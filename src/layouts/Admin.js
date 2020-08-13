720fba214b690cb7d8e7835c8dab4e5dcfcc8c23
jorgenholme <j.holme@stud.uis.no>
2020-08-13 17:05:48 +0200

    Remove footer

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -107,7 +107,6 @@ export default function Admin({ ...rest }) {
         ) : (
           <div className={classes.map}>{switchRoutes}</div>
         )}
-        {getRoute() ? <Footer /> : null}
       </div>
     </div>
   );
