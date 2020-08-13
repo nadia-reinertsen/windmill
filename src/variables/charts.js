771bb6038a3df1587e07760f857962ee26554078
nadia-reinertsen <nadia.reinertsen@bouvetno>
2020-08-13 11:58:13 +0200

    test

--- src/variables/charts.js
+++ src/variables/charts.js
@@ -17,15 +17,15 @@ var delays2 = 80,
 
 const dailySalesChart = {
   data: {
-    labels: ["M", "T", "W", "T", "F", "S", "S"],
-    series: [[12, 17, 7, 17, 23, 18, 38]]
+    labels: ["M", "T", "W", "T", "F", "S", "S", "F", "S", "S"],
+    series: [[12, 17, 7, 17, 23, 18, 38, 34, 78,3]]
   },
   options: {
     lineSmooth: Chartist.Interpolation.cardinal({
       tension: 0
     }),
     low: 0,
-    high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+    high: 25, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
     chartPadding: {
       top: 0,
       right: 0,
--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,52 +1,73 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // react plugin for creating charts
-import ChartistGraph from 'react-chartist';
+import ChartistGraph from "react-chartist";
 // @material-ui/core
-import { makeStyles } from '@material-ui/core/styles';
-import Icon from '@material-ui/core/Icon';
+import { makeStyles } from "@material-ui/core/styles";
+import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
-import Store from '@material-ui/icons/Store';
-import Warning from '@material-ui/icons/Warning';
-import DateRange from '@material-ui/icons/DateRange';
-import LocalOffer from '@material-ui/icons/LocalOffer';
-import Update from '@material-ui/icons/Update';
-import ArrowUpward from '@material-ui/icons/ArrowUpward';
-import AccessTime from '@material-ui/icons/AccessTime';
-import Accessibility from '@material-ui/icons/Accessibility';
-import BugReport from '@material-ui/icons/BugReport';
-import Code from '@material-ui/icons/Code';
-import Cloud from '@material-ui/icons/Cloud';
-import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
-import SpeedIcon from '@material-ui/icons/Speed';
-import ExploreIcon from '@material-ui/icons/Explore';
-import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
+import Store from "@material-ui/icons/Store";
+import Warning from "@material-ui/icons/Warning";
+import DateRange from "@material-ui/icons/DateRange";
+import LocalOffer from "@material-ui/icons/LocalOffer";
+import Update from "@material-ui/icons/Update";
+import ArrowUpward from "@material-ui/icons/ArrowUpward";
+import AccessTime from "@material-ui/icons/AccessTime";
+import Accessibility from "@material-ui/icons/Accessibility";
+import BugReport from "@material-ui/icons/BugReport";
+import Code from "@material-ui/icons/Code";
+import Cloud from "@material-ui/icons/Cloud";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import SpeedIcon from "@material-ui/icons/Speed";
+import ExploreIcon from "@material-ui/icons/Explore";
+import DoubleArrowIcon from "@material-ui/icons/DoubleArrow";
 
 // core components
-import GridItem from 'components/Grid/GridItem.js';
-import GridContainer from 'components/Grid/GridContainer.js';
-import Table from 'components/Table/Table.js';
-import Tasks from 'components/Tasks/Tasks.js';
-import CustomTabs from 'components/CustomTabs/CustomTabs.js';
-import Danger from 'components/Typography/Danger.js';
-import Card from 'components/Card/Card.js';
-import CardHeader from 'components/Card/CardHeader.js';
-import CardIcon from 'components/Card/CardIcon.js';
-import CardBody from 'components/Card/CardBody.js';
-import CardFooter from 'components/Card/CardFooter.js';
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Table from "components/Table/Table.js";
+import Tasks from "components/Tasks/Tasks.js";
+import CustomTabs from "components/CustomTabs/CustomTabs.js";
+import Danger from "components/Typography/Danger.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardIcon from "components/Card/CardIcon.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
 
 // graphs
 // import Graphs from '../Graphs/Graphs.js';
 
-import { bugs, website, server } from 'variables/general.js';
+import { bugs, website, server } from "variables/general.js";
 
-import { dailySalesChart, emailsSubscriptionChart, completedTasksChart } from 'variables/charts.js';
+import {
+  dailySalesChart,
+  emailsSubscriptionChart,
+  completedTasksChart,
+} from "variables/charts.js";
 
-import styles from 'assets/jss/material-dashboard-react/views/dashboardStyle.js';
+import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
 
 const useStyles = makeStyles(styles);
 
 export default function Dashboard() {
   //Erlend hjelper!!!
+
+  const windSpeedData = {
+    labels: ["10 hours ago ", " 9 hours ago ", " 8 hours ago ", "7 hours ago","6 hours ago","5 hours ago","4 hours ago","3 hours ago", "2 hours ago","1 hour ago"],
+    series: [[2, 17, 7, 17, 23, 18, 38]] //place array
+  }
+
+  const earningsData = {
+    labels: ["M", "T", "W", "T", "F", "S", "S"],
+    series: [[2, 17, 7, 17, 23, 18, 38]]
+  }
+
+  const activityData = {
+    labels: ["M", "T", "W", "T", "F", "S", "S"],
+    series: [[2, 17, 7, 17, 23, 18, 38]]
+  }
+
+
 
   const [windSpeed, setWindSpeed] = useState();
   const [moneyEarned, setMoneyEarned] = useState();
@@ -54,28 +75,28 @@ export default function Dashboard() {
   const [responses, setResponses] = useState([]);
 
   useEffect(() => {
-    fetch('https://vindafor.azurewebsites.net/api/Weather')
+    fetch("https://vindafor.azurewebsites.net/api/Weather")
       .then((response) => response.json())
       .then((data) => setWindSpeed(data));
 
-    fetch('https://vindafor.azurewebsites.net/api/PowerPrice')
+    fetch("https://vindafor.azurewebsites.net/api/PowerPrice")
       .then((response) => response.json())
       .then((data) => setMoneyEarned(data));
   }, [refresh]);
 
   useEffect(() => {
-    setTimeout(() => setRefresh(''), 60000);
+    setTimeout(() => setRefresh(""), 60000);
   }, [refresh]);
 
   useEffect(() => {
     //Atle hjelper
     let myheaders = {
-      GroupId: 'svg',
-      GroupKey: 'ZW43OAUPlEKuqfMETg0izA==',
+      GroupId: "svg",
+      GroupKey: "ZW43OAUPlEKuqfMETg0izA==",
     };
 
-    fetch('https://vindafor.azurewebsites.net/api/Windmills', {
-      method: 'GET',
+    fetch("https://vindafor.azurewebsites.net/api/Windmills", {
+      method: "GET",
 
       headers: myheaders,
     })
@@ -84,7 +105,7 @@ export default function Dashboard() {
   }, [refresh]);
 
   useEffect(() => {
-    setTimeout(() => setRefresh(''), 60000);
+    setTimeout(() => setRefresh(""), 60000);
   }, [refresh]);
 
   let activated = [];
@@ -101,13 +122,46 @@ export default function Dashboard() {
   if (windSpeed < 3 || windSpeed > 25) {
     currentEarnings = 0;
   } else if (windSpeed > 12) {
-    currentEarnings = -maintainanceCostConst - 76 * windSpeed + 4.8 * moneyEarned;
+    currentEarnings =
+      -maintainanceCostConst - 76 * windSpeed + 4.8 * moneyEarned;
   } else {
-    currentEarnings = -maintainanceCostConst - 76 * windSpeed + windSpeed * 0.4 * moneyEarned;
+    currentEarnings =
+      -maintainanceCostConst - 76 * windSpeed + windSpeed * 0.4 * moneyEarned;
   }
+
+  let activeWindmillsEarning = currentEarnings * activeSum;
+  
+  
+
+
   const classes = useStyles();
   return (
     <div>
+      <GridItem xs={3} sm={6} md={3}>
+        <Card>
+          <CardHeader color="danger" stats icon>
+            <CardIcon color="danger">
+              <SpeedIcon />
+            </CardIcon>
+
+            <p className={classes.cardCategory}>
+              Total earnings from current active windmills
+            </p>
+            <h3 className={classes.cardTitle}>
+            {Math.round(activeWindmillsEarning)} <small>NOK</small>
+            </h3>
+          </CardHeader>
+          <CardFooter stats>
+            <div className={classes.stats}>
+              <div className={classes.stats}>
+                <Update />
+                Updated 1 minute ago
+              </div>
+            </div>
+          </CardFooter>
+        </Card>
+      </GridItem>
+
       <GridContainer>
         <GridItem xs={12} sm={6} md={3}>
           <Card>
@@ -139,13 +193,14 @@ export default function Dashboard() {
               </CardIcon>
               <p className={classes.cardCategory}>Power price</p>
               <h3 className={classes.cardTitle}>
-                {moneyEarned} <small>kr</small>
+                {moneyEarned} <small>NOK</small>
               </h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
                 <DateRange />
                 Updated 1 minute ago
+                
               </div>
             </CardFooter>
           </Card>
@@ -156,9 +211,10 @@ export default function Dashboard() {
               <CardIcon color="info">
                 <ExploreIcon />
               </CardIcon>
-              <p className={classes.cardCategory}>Current earnings per hour</p> //
+              <p className={classes.cardCategory}>Current earnings per hour</p>{" "}
+              //
               <h3 className={classes.cardTitle}>
-                {Math.round(currentEarnings)} <small>kr</small>
+                {Math.round(currentEarnings)} <small>NOK</small>
               </h3>
             </CardHeader>
             <CardFooter stats>
@@ -176,7 +232,7 @@ export default function Dashboard() {
                 <DoubleArrowIcon />
               </CardIcon>
               <p className={classes.cardCategory}>Current windmills running</p>
-              <h3 className={classes.cardTitle}>{activeSum}</h3>
+              <h3 className={classes.cardTitle}>{activeSum}<small> Active</small></h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
@@ -193,24 +249,24 @@ export default function Dashboard() {
             <CardHeader color="success">
               <ChartistGraph
                 className="ct-chart"
-                data={dailySalesChart.data}
+                data={windSpeedData}
                 type="Line"
                 options={dailySalesChart.options}
                 listener={dailySalesChart.animation}
               />
             </CardHeader>
             <CardBody>
-              <h4 className={classes.cardTitle}>Daily Avg. Wind Speed (m/s)</h4>
+              <h4 className={classes.cardTitle}>Daily windspeed overview </h4>
               <p className={classes.cardCategory}>
                 <span className={classes.successText}>
-                  <ArrowUpward className={classes.upArrowCardCategory} /> 25%
-                </span>{' '}
+                  <ArrowUpward className={classes.upArrowCardCategory} /> 
+                </span>{" "}
                 Updated 1 minuite ago
               </p>
             </CardBody>
             <CardFooter chart>
               <div className={classes.stats}>
-                <AccessTime /> updated 4 minutes ago
+                <AccessTime /> Updated 1 minuite ago
               </div>
             </CardFooter>
           </Card>
@@ -228,8 +284,10 @@ export default function Dashboard() {
               />
             </CardHeader>
             <CardBody>
-              <h4 className={classes.cardTitle}>Monthly Avg. Earnings</h4>
-              <p className={classes.cardCategory}>Average Earnings From The Last 12 Months</p>
+              <h4 className={classes.cardTitle}>Daily Avg. Earnings</h4>
+              <p className={classes.cardCategory}>
+                Average Earnings From The Last 12 Months
+              </p>
             </CardBody>
             <CardFooter chart>
               <div className={classes.stats}>
@@ -250,7 +308,7 @@ export default function Dashboard() {
               />
             </CardHeader>
             <CardBody>
-              <h4 className={classes.cardTitle}>Completed Tasks</h4>
+              <h4 className={classes.cardTitle}>Daily Avg. Active windmills</h4>
               <p className={classes.cardCategory}>Last Campaign Performance</p>
             </CardBody>
             <CardFooter chart>
@@ -262,5 +320,7 @@ export default function Dashboard() {
         </GridItem>
       </GridContainer>
     </div>
+
+    
   );
 }
