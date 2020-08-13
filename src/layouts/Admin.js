7091c3952a1fb555d58d5810b589361145a40b04
jorgenholme <j.holme@stud.uis.no>
2020-08-13 16:39:41 +0200

    Change background image and logo

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -14,8 +14,8 @@ import routes from 'routes.js';
 
 import styles from 'assets/jss/material-dashboard-react/layouts/adminStyle.js';
 
-import bgImage from 'assets/img/sidebar-2.jpg';
-import logo from 'assets/img/reactlogo.png';
+import bgImage from 'assets/img/windmillbg.jfif';
+import logo from 'assets/img/wind_logo.webp';
 
 let ps;
 
